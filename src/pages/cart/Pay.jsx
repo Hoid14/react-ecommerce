2bc12007878614652ca00d109a71721a0a0a04ed
Sergio Arias <s-andres140@hotmail.com>
2024-04-03 00:14:24 -0500

    actualizacion

--- src/pages/cart/Pay.jsx
+++ src/pages/cart/Pay.jsx
@@ -7,7 +7,7 @@ import './pay.css'
 import { useNavigate } from "react-router-dom"
 export const Pay = () => {
     
-    const {cartItems, getTotalCartAmount, resetCart, getTotalItems} =useContext(ShopContext)
+    const {cartItems, getTotalCartAmount, resetCart} =useContext(ShopContext)
 
     const navigate = useNavigate()
     const totalAmount = getTotalCartAmount()
